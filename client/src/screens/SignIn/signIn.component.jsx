c100bf996d4a300e5cf07926b283f819c2bfdb45
DenisBukva <denisbukva@hotmail.com>
2022-10-06 12:33:20 +0200

    add signIn component

--- client/src/screens/SignIn/signIn.component.jsx
+++ client/src/screens/SignIn/signIn.component.jsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Button, Row, Col, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
 
-const SignIn = () => {
+import Message from '../../components/Message/message.component';
+import Loader from '../../components/Loader/loader.component';
+import FormContainer from '../../components/FormContainer/formContainer.component';
+import { login } from '../../store/actions/user.actions';
+
+const defaultFormFields = {
+    email: '',
+    password: ''
+}
+
+const SignIn = ({ location, history }) => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { email, password } = formFields;
+
+    const dispatch = useDispatch();
+    const userLogin = useSelector(state => state.userLogin);
+    const { loading, error, userInfo } = userLogin;
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(login(email, password));
+        resetFormFields();
+    };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setFormFields({ ...formFields, [name]: value })
+    };
+
     return (
-        <div>
-            <h1>This is Sign In Page</h1>
-        </div>
+        <FormContainer>
+            <h1>Sign In</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+                <FormGroup controlId='email'>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl
+                        type='email'
+                        placeHolder='Enter Email'
+                        required
+                        onChange={handleChange}
+                        value={email}
+                        name='email'>
+                    </FormControl>
+                </FormGroup>
+                <FormGroup controlId='password'>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl
+                        type='password'
+                        placeHolder='Enter Password'
+                        required
+                        onChange={handleChange}
+                        value={password}
+                        name='password'>
+                    </FormControl>
+                </FormGroup>
+
+                <Button className='mt-4' type='submit' variant='primary'>
+                    Sign In
+                </Button>
+            </Form>
+            <Row className='py-3'>
+                <Col>
+                    Don't have an account?{' '}
+                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+                        Sign Up
+                    </Link>
+                </Col>
+            </Row>
+        </FormContainer>
     )
 }
 
