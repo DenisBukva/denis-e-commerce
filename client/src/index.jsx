5b9049e8e2b7fc8cb41e5f517af4267d4f3dd439
DenisBukva <denisbukva@hotmail.com>
2022-10-06 16:04:07 +0200

    add HelmetProvider to index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 
 import store from './store/store';
 import App from './App';
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Provider>
 
   </React.StrictMode>
