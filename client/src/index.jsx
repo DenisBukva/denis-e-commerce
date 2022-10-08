2cc9932f38d65978dff1c6b596de28f98f7cc28e
DenisBukva <denisbukva@hotmail.com>
2022-10-08 13:20:55 +0200

    add styles tamplate

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import store from './store/store';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css'
 import './index.css';
 
 
