4f96a9358c0ef4122ebed7654ab195bfc4faa7df
DenisBukva <denisbukva@hotmail.com>
2022-10-10 14:18:21 +0200

    add route for ProductScreen

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -5,6 +5,7 @@ import Navigation from './screens/Navigation/navigation.component';
 import HomeScreen from './screens/Home/home.component';
 import SignIn from './screens/SignIn/signIn.component';
 import SignUp from './screens/SignUp/signUp.component';
+import ProductScreen from './screens/ProductScreen/productScreen.component';
 
 import Footer from './components/Footer/footer.component';
 
@@ -14,6 +15,7 @@ const MainRouter = () => {
       <Navigation />
       <main className='py-3'>
         <Container>
+          <Route path='/product/:id' component={ProductScreen} />
           <Route path='/search/:keyword' component={HomeScreen} exact />
           <Route path='/page/:pageNumber' component={HomeScreen} exact />
           <Route
